9e95f079186ba1a7b114da076d8cfa574c75e999
Isaac Williams <isaacwilliams@Isaac-Williamss-MacBook-Pro.local>
2019-02-03 22:03:04 +0100

    Can now use station name to find file

--- opendap.py
+++ opendap.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import os
+import netCDF4
 
 
 def get_files(url):
@@ -16,10 +17,14 @@ def get_files(url):
     return files
 
 
-BASE_URL = 'http://opendap.deltares.nl/thredds/catalog/opendap/rijkswaterstaat/waterbase'
+CATALOG_URL = ('http://opendap.deltares.nl/thredds/'
+               'catalog/opendap/rijkswaterstaat/waterbase')
+
+DATA_URL = ('http://opendap.deltares.nl/thredds/'
+            'dodsC/opendap/rijkswaterstaat/waterbase/')
 
 MEASUREMENTS = []
-for file in get_files(os.path.join(BASE_URL, 'catalog.html')):
+for file in get_files(os.path.join(CATALOG_URL, 'catalog.html')):
     MEASUREMENTS.append({
         'id': re.findall('(^\d+)', file)[0],
         'parameter': re.findall('^\d+_(.*)', file)[0]
@@ -46,15 +51,37 @@ class Measurement(object):
         self.measurement = measurement
         self.full_name = '_'.join((measurement_id, measurement))
 
-        self.catalog_url = os.path.join(BASE_URL, self.full_name, 'nc', 'catalog.html')
+        self.catalog_url = os.path.join(CATALOG_URL, self.full_name, 'nc', 'catalog.html')
         self.files = get_files(self.catalog_url)
 
-    def get_available_stations(self):
+    def get_station_codes(self):
         pattern = '^id\d+-(.*).nc$'
-        stations = map(lambda file: re.search(pattern, file).group(), self.files)
-        return list(stations)
+        return [re.findall(pattern, file)[0] for file in self.files]
 
     def get_file_path(self, file):
         if file not in self.files:
-            return ValueError("File doesn't exist.")
-        return os.path.join(BASE_URL, self.full_name, 'nc', file)
+            raise ValueError("File Doesn't e")
+        return os.path.join(DATA_URL, self.full_name, 'nc', file)
+
+    def get_station_file(self, station_name):
+        file_out = None
+        station_name = station_name.lower()
+        for code, file in zip(self.get_station_codes(), self.files):
+            if all([letter in station_name for letter in code.lower()]):
+                file_path = self.get_file_path(file)
+                with netCDF4.Dataset(file_path, 'r') as ds:
+                    if ds.stationname.lower() == station_name.lower():
+                        file_out = file_path
+        return file_out
+
+    def get_all_available_stations(self):
+        pattern = '^id\d+-(.*).nc$'
+        stations = []
+        for file in self.files:
+            file_path = self.get_file_path(file)
+            with netCDF4.Dataset(file_path, 'r') as ds:
+                stations.append({
+                    'full_name': ds.stationname,
+                    'short_name': re.findall(pattern, file)[0]
+                })
+        return stations
