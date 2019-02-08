00686a2a61e7ee1a0b9db0bc9186c75bf2fd52ef
Isaac Williams <isaacwilliams@Isaac-Williamss-MacBook-Pro.local>
2019-02-08 20:07:58 +0100

    minor changes

--- opendap.py
+++ opendap.py
@@ -6,6 +6,13 @@ import os
 import netCDF4
 
 
+CATALOG_URL = ('http://opendap.deltares.nl/thredds/'
+               'catalog/opendap/rijkswaterstaat/waterbase')
+
+DATA_URL = ('http://opendap.deltares.nl/thredds/'
+            'dodsC/opendap/rijkswaterstaat/waterbase/')
+
+
 def get_files(url):
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
@@ -16,12 +23,6 @@ def get_files(url):
             files.append(item.text.strip('/'))
     return files
 
-
-CATALOG_URL = ('http://opendap.deltares.nl/thredds/'
-               'catalog/opendap/rijkswaterstaat/waterbase')
-
-DATA_URL = ('http://opendap.deltares.nl/thredds/'
-            'dodsC/opendap/rijkswaterstaat/waterbase/')
 
 MEASUREMENTS = []
 for file in get_files(os.path.join(CATALOG_URL, 'catalog.html')):
@@ -50,38 +51,69 @@ class Measurement(object):
 
         self.measurement = measurement
         self.full_name = '_'.join((measurement_id, measurement))
+        self.catalog_url = os.path.join(CATALOG_URL, self.full_name,
+                                        'nc', 'catalog.html')
 
-        self.catalog_url = os.path.join(CATALOG_URL, self.full_name, 'nc', 'catalog.html')
-        self.files = get_files(self.catalog_url)
+    @property
+    def files(self):
+        files = [os.path.join(DATA_URL, self.full_name, 'nc', file)
+                     for file in get_files(self.catalog_url)]
+        return files
 
     def get_station_codes(self):
-        pattern = '^id\d+-(.*).nc$'
+        pattern = 'id\d+-(.*).nc$'
         return [re.findall(pattern, file)[0] for file in self.files]
-
-    def get_file_path(self, file):
-        if file not in self.files:
-            raise ValueError("File Doesn't e")
-        return os.path.join(DATA_URL, self.full_name, 'nc', file)
 
     def get_station_file(self, station_name):
         file_out = None
         station_name = station_name.lower()
         for code, file in zip(self.get_station_codes(), self.files):
             if all([letter in station_name for letter in code.lower()]):
-                file_path = self.get_file_path(file)
-                with netCDF4.Dataset(file_path, 'r') as ds:
+                with netCDF4.Dataset(file, 'r') as ds:
                     if ds.stationname.lower() == station_name.lower():
-                        file_out = file_path
+                        file_out = file
         return file_out
 
     def get_all_available_stations(self):
-        pattern = '^id\d+-(.*).nc$'
         stations = []
         for file in self.files:
-            file_path = self.get_file_path(file)
             with netCDF4.Dataset(file_path, 'r') as ds:
                 stations.append({
-                    'full_name': ds.stationname,
-                    'short_name': re.findall(pattern, file)[0]
+                    'station_name': ds.stationname,
+                    'location_code': ds.locationcode,
+                    'coordinates': [ds.geospatial_lat_min, ds.geospatial_lon_min],
+                    'crs': 'epsg:4326'
                 })
         return stations
+
+
+class File:
+
+    def __init__(self, filepath):
+        self.filepath = self.filepath
+
+    def get_file_meta(self):
+        with self.open() as ds:
+            station = {
+                'station_name': ds.stationname,
+                'location_code': ds.locationcode,
+                'coordinates': [ds.geospatial_lat_min, ds.geospatial_lon_min],
+                'crs': 'epsg:4326'
+            }
+        return station
+
+    def get_data(self, start_date, end_date):
+        with self.open() as ds:
+            t = ds.variables['time']
+        return t
+
+    def open(self):
+        return netCDF4.Dataset(self.file_path, 'r')
+
+
+if __name__ == "__main__":
+    print(MEASUREMENTS)
+    measurement = 'Totaal_fosfaat_in_mg_l_na_filtratie_in_oppervlaktewater'
+    x = Measurement(measurement)
+    print(x.get_station_codes())
+    print(x.files)
