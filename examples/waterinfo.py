e07d513f3908ab8003d929113bf3c53ecf140cdc
iw108 <isaac.andrew.williams@gmail.com>
2019-07-03 20:54:05 +0200

    updated waterinfo examples

--- examples/waterinfo.py
+++ examples/waterinfo.py
@@ -1,5 +1,7 @@
 
-from datetime import datetime
+from datetime import datetime, timedelta
+
+from matplotlib import pyplot as plt
 
 from waterinfo.public import Waterhoogte
 
@@ -7,16 +9,21 @@ from waterinfo.public import Waterhoogte
 waterhoogte = Waterhoogte()
 station = waterhoogte.stations[0]
 
-start_date = datetime(2019, 2, 1)
-end_date = datetime(2019, 2, 3)
+start_date = datetime.now() - timedelta(days=3)
+end_date = datetime.now() - timedelta(days=1)
+
+
+data_from_horizon = waterhoogte.get_data_from_horizon(
+    station['name'], start_date, end_date
+)
+print(data_from_horizon)
+
 
 data_between_dates = waterhoogte.get_data_between_dates(
     station['name'], start_date, end_date, parse=True
 )
 
-data_from_horizon = waterhoogte.get_data_from_horizon(
-    station['name'], start_date, end_date
-)
-
-waterhoogte.update_station_crs('epsg:4326')
-print(data_from_horizon)
+plt.figure()
+plt.plot(data_between_dates)
+plt.ylabel('tidal level (cm)')
+plt.xlabel('date')
